6925021f38dadd06e68a83fb308821d7235f6c88
Erl-koenig <knoefler.ivan@gmail.com>
2023-09-06 21:48:14 +0200

    reverted to original version

--- main.ts
+++ main.ts
@@ -1,71 +1,41 @@
-import { Plugin, MarkdownView, Editor } from 'obsidian';
+import { Plugin, MarkdownView, Editor } from "obsidian";
 
 export default class CheckboxReorderPlugin extends Plugin {
-  async onload() {
-    console.log('Checkbox Reorder Plugin loaded');
-    
-    this.addCommand({
-      id: 'reorder-checkboxes',
-      name: 'Reorder Checkboxes',
-      editorCallback: (editor: Editor, view: MarkdownView) => {
-        this.reorderCheckboxes(editor);
-      }
-    });
-  }
+	async onload() {
+		console.log("Checkbox Reorder Plugin loaded");
 
-  reorderCheckboxes(editor: Editor) {
-    const currentText = editor.getValue();
-    
-    // Regular expression to match checkboxes including nested ones
-    const checkboxPattern = /^(\s*)- \[(x| )\] .+$/gm;
-    
-    // Extract all checkboxes from the text
-    const checkboxes = Array.from(currentText.matchAll(checkboxPattern), match => ({
-      indent: match[1].length, // Indentation level
-      text: match[0] // Full match text
-    }));
-    
-    // Separate checked and unchecked checkboxes
-    const uncheckedCheckboxes = checkboxes.filter(cb => cb.text.includes('[ ]'));
-    const checkedCheckboxes = checkboxes.filter(cb => cb.text.includes('[x]'));
+		this.addCommand({
+			id: "reorder-checkboxes",
+			name: "Reorder Checkboxes in List",
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				this.reorderCheckboxes(editor);
+			},
+		});
+	}
 
-    // Filter and sort checked checkboxes
-    const sortedCheckboxes = this.sortCheckedCheckboxes(checkedCheckboxes);
-    
-    // Reconstruct the reordered text
-    const reorderedText = [
-      ...uncheckedCheckboxes.map(cb => cb.text),
-      ...sortedCheckboxes.map(cb => cb.text)
-    ].join('\n');
-  
-    // Replace the content in the editor
-    editor.setValue(reorderedText);
-  }
+	reorderCheckboxes(editor: Editor) {
+		const currentText = editor.getValue();
 
-  sortCheckedCheckboxes(checkedCheckboxes) {
-    const sortedCheckboxes = [];
-    const toMove = [];
+		const checkboxPattern = /^- \[(x| )\] .+$/gm;
 
-    for (const checkbox of checkedCheckboxes) {
-      const indent = checkbox.indent;
-      let hasIncompleteNested = false;
+		const uncheckedCheckboxes =
+			currentText
+				.match(checkboxPattern)
+				?.filter((cb) => cb.startsWith("- [ ]")) || [];
+		const checkedCheckboxes =
+			currentText
+				.match(checkboxPattern)
+				?.filter((cb) => cb.startsWith("- [x]")) || [];
 
-      for (const nextCheckbox of checkedCheckboxes) {
-        if (nextCheckbox.indent <= indent) break;
+		const reorderedCheckboxes = [
+			...uncheckedCheckboxes,
+			...checkedCheckboxes,
+		];
+		const reorderedText = currentText.replace(
+			checkboxPattern,
+			() => reorderedCheckboxes.shift() || "",
+		);
 
-        if (nextCheckbox.text.includes('[ ]')) {
-          hasIncompleteNested = true;
-          break;
-        }
-      }
-
-      if (!hasIncompleteNested) {
-        sortedCheckboxes.push(checkbox);
-      } else {
-        toMove.push(checkbox);
-      }
-    }
-
-    return sortedCheckboxes.concat(toMove);
-  }
+		editor.setValue(reorderedText);
+	}
 }
