4ad8daf9f023ead8200a353513f1be4ce1191f74
Erl-koenig <knoefler.ivan@gmail.com>
2024-03-04 14:45:51 +0100

    added option for sortin (top / bottom)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
-import { Plugin, MarkdownView, Editor } from "obsidian";
+import {
+	Plugin,
+	MarkdownView,
+	Editor,
+	WorkspaceLeaf,
+	App,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
 import { reorderCheckboxesInFile } from "src/reorderCheckboxes";
 
+interface CheckboxReorderSettings {
+	sortTop: boolean;
+}
+
+const DEFAULT_SETTINGS: CheckboxReorderSettings = {
+	sortTop: true,
+};
+
 export default class CheckboxReorderPlugin extends Plugin {
+	settings: CheckboxReorderSettings;
+
 	async onload() {
+		await this.loadSettings();
+
 		console.log("Checkbox Reorder Plugin loaded");
 
 		this.addCommand({
@@ -12,11 +32,63 @@ export default class CheckboxReorderPlugin extends Plugin {
 				this.reorderCheckboxes(editor);
 			},
 		});
+
+		this.addSettingTab(new CheckboxReorderSettingsTab(this.app, this));
+	}
+
+	onunload() {
+		console.log("Checkbox Reorder Plugin unloaded");
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData(),
+		);
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
 	}
 
 	reorderCheckboxes(editor: Editor) {
 		const currentText = editor.getValue();
-		const reorderedText = reorderCheckboxesInFile(currentText);
+		const reorderedText = reorderCheckboxesInFile(
+			currentText,
+			this.settings.sortTop,
+		);
 		editor.setValue(reorderedText);
+	}
+}
+
+class CheckboxReorderSettingsTab extends PluginSettingTab {
+	plugin: CheckboxReorderPlugin;
+
+	constructor(app: App, plugin: CheckboxReorderPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		let { containerEl } = this;
+
+		containerEl.empty();
+
+		containerEl.createEl("h2", { text: "Checkbox Reorder Settings" });
+
+		new Setting(containerEl)
+			.setName("Top / Bottom")
+			.setDesc(
+				"On = Top, Off = Bottom. Choose whether to sort checkboxes at the top or bottom.",
+			)
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.sortTop)
+					.onChange(async (value) => {
+						this.plugin.settings.sortTop = value;
+						await this.plugin.saveSettings();
+					}),
+			);
 	}
 }
--- src/reorderCheckboxes.ts
+++ src/reorderCheckboxes.ts
@@ -1,33 +1,34 @@
+function reorderCheckboxesInList(inputText: string, moveUp: boolean) {
+	const checkboxPattern = /^- \[(x| )\] .+(\n[ \t]+-.*)*$/gm;
 
-function reorderCheckboxesInList(inputText: string) {
-  const checkboxPattern = /^- \[(x| )\] .+(\n[ \t]+-.*)*$/gm;
+	const allCheckboxes = inputText.match(checkboxPattern) ?? [];
 
-  const allCheckboxes = inputText.match(checkboxPattern) ?? [];
+	const uncheckedCheckboxes = allCheckboxes.filter((cb) =>
+		cb.startsWith("- [ ]"),
+	);
+	const checkedCheckboxes = allCheckboxes.filter((cb) =>
+		cb.startsWith("- [x]"),
+	);
 
-  const uncheckedCheckboxes =
-    allCheckboxes
-      .filter((cb) => cb.startsWith("- [ ]"));
-  const checkedCheckboxes =
-    allCheckboxes
-      .filter((cb) => cb.startsWith("- [x]"));
+	let reorderedCheckboxes: string[];
+	if (moveUp) {
+		reorderedCheckboxes = [...uncheckedCheckboxes, ...checkedCheckboxes];
+	} else {
+		reorderedCheckboxes = [...checkedCheckboxes, ...uncheckedCheckboxes];
+	}
 
-  const reorderedCheckboxes = [
-    ...uncheckedCheckboxes,
-    ...checkedCheckboxes,
-  ];
-  const reorderedText = inputText.replace(
-    checkboxPattern,
-    () => reorderedCheckboxes.shift() || "",
-  );
+	const reorderedText = inputText.replace(
+		checkboxPattern,
+		() => reorderedCheckboxes.shift() || "",
+	);
 
-  return reorderedText;
+	return reorderedText;
 }
 
-export function reorderCheckboxesInFile(inputText: string) {
-  const checkboxListPattern = /^- .+(\n[ \t]*- .*)*$/gm;
-  const reorderedText = inputText.replace(
-    checkboxListPattern,
-    reorderCheckboxesInList
-  );
-  return reorderedText;
+export function reorderCheckboxesInFile(inputText: string, moveUp: boolean) {
+	const checkboxListPattern = /^- .+(\n[ \t]*- .*)*$/gm;
+	const reorderedText = inputText.replace(checkboxListPattern, (match) =>
+		reorderCheckboxesInList(match, moveUp),
+	);
+	return reorderedText;
 }
